dd697a5381bb555652d087767e8c1fb867cc0ab8
satar <abdisatar.mohamed@pixeledge.io>
2024-04-12 01:35:51 +0300

    feat: add handler to toggle

--- src/components/TodoBlock/index.tsx
+++ src/components/TodoBlock/index.tsx
@@ -1,41 +1,47 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
+import {TodoContext} from '../../contexts/TodoProvider';
 import {Checked, Container, Radio, Todo} from './styles';
-import {Alert} from 'react-native';
 
 export function TodoBlock(): React.ReactNode {
+  const {todos, saveTodos} = useContext(TodoContext);
+  const handleToggleTodo = (id: number) => {
+    // Alert.alert(id + '');
+    const tempTodo = todos;
+    let currentTodo = tempTodo[id];
+    tempTodo[id] = {...currentTodo, completed: !currentTodo.completed};
+    saveTodos([...tempTodo]);
+  };
+
   return (
     <>
-      <Container onPress={() => Alert.alert('Pressed')}>
-        <Radio>
-          <Checked> &#x2713;</Checked>
-        </Radio>
-        <Todo>Text</Todo>
-      </Container>
-      <Container>
-        <Radio>
-          <Checked> &#x2713;</Checked>
-        </Radio>
-        <Todo>Text</Todo>
-      </Container>
-      <Container>
-        <Radio>
-          <Checked> &#x2713;</Checked>
-        </Radio>
-        <Todo>Text</Todo>
-      </Container>
-      <Container>
-        <Radio>
-          <Checked> &#x2713;</Checked>
-        </Radio>
-        <Todo>Text</Todo>
-      </Container>
-      <Container>
-        <Radio>
-          <Checked> &#x2713;</Checked>
-        </Radio>
-        <Todo>Text</Todo>
-      </Container>
+      {!!todos.length &&
+        todos.map(({id, text, completed}, index) => {
+          return (
+            <Container onPress={() => handleToggleTodo(index)} key={text + id}>
+              {completed && (
+                <>
+                  <Radio>
+                    <Checked> &#x2713;</Checked>
+                  </Radio>
+                  <Todo
+                    style={{
+                      textDecorationLine: 'line-through',
+                      textDecorationStyle: 'solid',
+                    }}>
+                    {text}
+                  </Todo>
+                </>
+              )}
+              {!completed && (
+                <>
+                  <Radio style={{backgroundColor: 'transparent'}} />
+                  <Todo>{text}</Todo>
+                </>
+              )}
+            </Container>
+          );
+        })}
     </>
   );
 }
